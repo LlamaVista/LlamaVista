809e1e65479f6cd5cb6edc0cce09eef956522456
Choi Seunghun <0015chseang@gmail.com>
2023-10-31 21:36:39 +0900

    feat: cache annotation 추가

--- spring/src/main/java/hoon/capstone/llama/LlamaApplication.java
+++ spring/src/main/java/hoon/capstone/llama/LlamaApplication.java
@@ -2,8 +2,10 @@ package hoon.capstone.llama;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class LlamaApplication {
 
 	public static void main(String[] args) {
