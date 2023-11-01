c1e5b50e46efd332f375033dc54eae2b2a981453
Choi Seunghun <0015chseang@gmail.com>
2023-11-01 12:08:34 +0900

    fix: response 받은 뒤에 fetch

--- spring/src/main/java/hoon/capstone/llama/service/FetchService.java
+++ spring/src/main/java/hoon/capstone/llama/service/FetchService.java
@@ -3,6 +3,8 @@ package hoon.capstone.llama.service;
 import hoon.capstone.llama.domain.ModelOutput;
 import hoon.capstone.llama.repository.ModelOutputRepository;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,7 +24,11 @@ public class FetchService {
 
     public ModelOutput fetchData(String blobName) {
         String url = flaskEndpoint + "?blob_name=" + blobName;
-        restTemplate.getForEntity(url, String.class);
-        return modelOutputRepository.findById(1L).orElse(null);
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        if (response.getStatusCode() == HttpStatus.OK) {
+            return modelOutputRepository.findById(1L).orElse(null);
+        } else {
+            return null;
+        }
     }
 }
